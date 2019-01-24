75d1b90e2886760e8814a98ee73972efb04d82ea
SplendidX <splendidx@cocaine.ninja>
2019-01-24 03:02:19 +0200

    less redundant

--- lib/exsimplecheck.ex
+++ lib/exsimplecheck.ex
@@ -5,24 +5,20 @@ defmodule Exsimplecheck do
       |> :crypto.strong_rand_bytes()
       |> Base.url_encode64(padding: false)
 
-    IO.puts("Checking #{user}..")
     user
   end
 
   def get(user) do
     case(HTTPoison.get("https://api.mojang.com/users/profiles/minecraft/#{user}")) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> body
-      {:ok, %HTTPoison.Response{status_code: 204}} -> body = nil
+      {:ok, %HTTPoison.Response{status_code: 200}} -> IO.puts("#{user} taken")
+      {:ok, %HTTPoison.Response{status_code: 204}} -> IO.puts("#{user} open")
       _ -> :error
     end
   end
 
-  def process_body(nil), do: IO.puts("Username available!!!")
-  def process_body(body), do: IO.puts("Username taken...")
-
   def check() do
     :timer.sleep(1000)
-    Exsimplecheck.gen_alphanumeric() |> Exsimplecheck.get() |> Exsimplecheck.process_body()
+    Exsimplecheck.gen_alphanumeric() |> Exsimplecheck.get()
     check()
   end
 end
